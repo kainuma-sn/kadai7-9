d6f0ad8dc48959229d41086fc2616709bf24c622
Shunta Kainuma <arias230461@gmail.com>
2023-10-15 21:21:30 +0900

    HTTPからGETでMySQLのデータが取得できる環境を構築

--- sql/catalog_list.sql
+++ sql/catalog_list.sql
@@ -2,12 +2,13 @@ DROP TABLE IF EXISTS catalog_list;
 
 CREATE TABLE catalog_list (
  id int unsigned AUTO_INCREMENT,
- name VARCHAR(50) NOT NULL,
- price int unsigned NOT NULL,
+ productName VARCHAR(50) NOT NULL,
+ productPrice int unsigned NOT NULL,
+ productStock int unsigned NOT NULL,
  PRIMARY KEY(id)
 );
 
-INSERT INTO names (id, name, price) VALUES (1, "SUPERIOR DRUMMER 3", 49500, 50);
-INSERT INTO names (id, name, price) VALUES (2, "BFD 3",35799,37);
-INSERT INTO names (id, name, price) VALUES (3, "MODO DRUM 1.5",30990,25);
-INSERT INTO names (id, name, price) VALUES (4, "TOKYO SCORING DRUM KITS", 44671, 0);
+INSERT INTO catalog_list (id, productName, productPrice, productStock) VALUES (1,"SUPERIOR DRUMMER 3", 49500, 50);
+INSERT INTO catalog_list (id, productName, productPrice, productStock) VALUES (2,"BFD 3",35799,37);
+INSERT INTO catalog_list (id, productName, productPrice, productStock) VALUES (3,"MODO DRUM 1.5",30990,25);
+INSERT INTO catalog_list (id, productName, productPrice, productStock) VALUES (4,"TOKYO SCORING DRUM KITS", 44671, 10);
